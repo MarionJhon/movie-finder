ba6f292ec5d278188f831c1cc806440831d1dbee
Marion Jhon Agapay <marionagapay2001@gmail.com>
2025-09-18 22:31:42 +0800

    refactor: change the DATABASES to TableDB and the documents to rows, and and the query limit to one

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,29 +1,31 @@
-import { Client, Databases, ID, Query } from "appwrite";
+import { Client, TablesDB, ID, Query } from "appwrite";
 
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
-const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const TABLE_ID = import.meta.env.VITE_APPWRITE_TABLE_ID;
 
 const client = new Client()
   .setEndpoint("https://syd.cloud.appwrite.io/v1")
   .setProject(PROJECT_ID);
 
-const database = new Databases(client);
+const tables = new TablesDB(client);
 
 export const updateSearchCount = async (searchTerm, movie) => {
   try {
-    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+    // Use listRows instead of listDocuments
+    const result = await tables.listRows(DATABASE_ID, TABLE_ID, [
       Query.equal("searchTerm", searchTerm),
+      Query.limit(1), // only need one row
     ]);
 
-    if (result.documents.length > 0) {
-      const doc = result.documents[0];
+    if (result.total > 0) {
+      const row = result.rows[0];
 
-      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
-        count: doc.count + 1,
+      await tables.updateRow(DATABASE_ID, TABLE_ID, row.$id, {
+        count: row.count + 1,
       });
     } else {
-      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+      await tables.createRow(DATABASE_ID, TABLE_ID, ID.unique(), {
         searchTerm,
         count: 1,
         movie_id: movie.id,
@@ -31,18 +33,17 @@ export const updateSearchCount = async (searchTerm, movie) => {
       });
     }
   } catch (e) {
-    console.error(e);
+    console.error("Error updating search count:", e);
   }
-  console.log(PROJECT_ID, DATABASE_ID, COLLECTION_ID);
 };
 
 export const getTrendingMovies = async () => {
   try {
-    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+    const result = await tables.listRows(DATABASE_ID, TABLE_ID, [
       Query.limit(5),
       Query.orderDesc("count"),
     ]);
-    return result.documents;
+    return result.rows;
   } catch (error) {
     console.error(error);
   }
